f085282754db74675c40c684b344584c656aa93c
kong0107 <kong0107@gmail.com>
2018-12-04 05:15:52 +0800

    detect formula and table, and then set warning

--- lawtext2obj.js
+++ lawtext2obj.js
@@ -24,6 +24,14 @@ const getStratum = text => {
  *
  * 所得稅法第14條第1項那種「類」裡面還有分「段」的，先加個換行然後併進前一個元素裡。
  * 該項第四類各款後還有一段，就會被錯誤地歸到該類第三款裡頭，不過先不管了。
+ *
+ * 警告類型：（可能會互相覆蓋，但先不管了）
+ * * fullLine: 該行雖是句號結尾，所以不知道下一行是否是新的一「項」。
+ * * formula: 算式。但未必有等號或分號符號，例如全民健康保險藥物給付項目及支付標準第75條第3款第2目
+ *   @see {@link https://unicode-table.com/en/blocks/mathematical-operators/ }
+ *   @see {@link https://unicode-table.com/en/blocks/halfwidth-and-fullwidth-forms/ }
+ * * table: 表格。例如用戶用電設備裝置規則第62條
+ *   @see {@link https://unicode-table.com/en/blocks/box-drawing/ }
  */
 const text2paras = text => {
     const lines = text.trimEnd().split("\n").map(str => str.trimEnd());
@@ -31,12 +39,20 @@ const text2paras = text => {
     let paras = []; // 不管層級的項款目陣列
     let curPara = "";   // 處理中的項款目，不含空格，用於輸出
     let firstLineInPara = ""; // 處理中的項款目的第一行原始文字，包含空格，用於判斷層級，以及是不是所得稅法第14條的情形。
+    let warning; // 紀錄可能需要警告的情形，於新的段落時重置。
+    let raw = []; // 紀錄原本是哪幾行字，於有警告的時候一併回傳。
 
     lines.forEach(line => {
         if(!curPara) firstLineInPara = line;
         curPara += line.trimStart();
+        raw.push(line);
+        if(/[\u2500-\u257F]/.test(line)) warning = "table";
+        if(/[\u2200-\u22FF＊＋－／＜＝＞]/.test(line)) warning = "formula";
+
+        // 如果這一行「不是」項款目的結束，那就換下一行處理。
         if(!/(：|︰|。|（刪除）)$/.test(line)) return;
 
+        // 處理「這一個項款目結束了」的情形。
         const stratum = getStratum(firstLineInPara);
         if(stratum < 0) paras[paras.length - 1].text += "\n" + curPara;
         else paras.push({
@@ -45,11 +61,17 @@ const text2paras = text => {
             children: []
         });
 
+        const lastPara = paras[paras.length - 1];
         // 有可能該行雖是句號結尾，但其實還沒有要分項，因此標示警告。
-        if(line.length == 32 && line.charAt(31) == "。")
-            paras[paras.length - 1].warning = true;
+        if(!warning && line.length == 32 && line.charAt(31) == "。") warning = "fullLine";
+        if(warning) {
+            lastPara.warning = warning;
+            lastPara.raw = raw;
+        }
 
         curPara = "";
+        warning = false;
+        raw = [];
     });
 
     return paras;
